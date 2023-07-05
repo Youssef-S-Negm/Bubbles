f4e22ea1a0095be8be86b29e81c1606f0efede3f
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-06 01:18:18 +0300

    Sign in screen

---
+++ components/screens/SignInScreen.js
@@ -0,0 +1,77 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+
+const SignInScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.greetingText}>Welcome to Bubbles!</Text>
+            <Text style={styles.normalText}>Please enter your credentials</Text>
+            <TextInput
+                placeholder='user@example.com'
+                keyboardType='email-address'
+                textAlign='center'
+                style={styles.textInput}
+                value={email}
+                onChangeText={e => setEmail(e)}
+            />
+            <View style={{ height: 16 }}></View>
+            <TextInput
+                placeholder='Password'
+                secureTextEntry={true}
+                textAlign='center'
+                style={styles.textInput}
+                value={password}
+                onChangeText={(e) => setPassword(e)}
+            />
+            <View style={styles.forgotPasswordView}>
+                <Text
+                    style={styles.forgotPasswordText}
+                    onPress={() => {
+                        //TODO
+                    }}
+                >
+                    forgot password?
+                </Text>
+            </View>
+            <Button title='Sign in' />
+            <Text style={styles.normalText}>or</Text>
+            <Button title='Sign in with Google' />
+        </View>
+    )
+}
+
+export default SignInScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    greetingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        padding: 8
+    },
+    normalText: {
+        fontSize: 24,
+        padding: 8
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        borderWidth: 1,
+        padding: 8,
+        borderRadius: 6,
+    },
+    forgotPasswordText: {
+        textAlign: 'left',
+        color: 'blue',
+        textDecorationLine: 'underline'
+    },
+    forgotPasswordView: {
+        alignSelf: 'stretch',
+        paddingTop: 4
+    }
+})

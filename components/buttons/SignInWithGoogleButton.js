c8c2d9c84cd309bc7f1ae1eb3a6ae8ff74653a06
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-06 22:13:04 +0300

    sign In with Google button component

---
+++ components/buttons/SignInWithGoogleButton.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import React from 'react'
+
+const SignInWithGoogleButton = () => {
+    return (
+        <TouchableOpacity
+            onPress={() => {
+                //TODO
+            }}
+        >
+            <View style={styles.container}>
+                <Image
+                    source={require('../../assets/googleIcon.png')}
+                    style={styles.imgae}
+                />
+                <View style={{ paddingVertical: 8 }}>
+                    <Text style={styles.text}>Sign In with Google</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default SignInWithGoogleButton
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderRadius: 6,
+        borderWidth: 1,
+        paddingLeft: 8
+    },
+    imgae: {
+        height: 35,
+        width: 35,
+        backgroundColor: '#fff',
+        borderRadius: 6
+    },
+    text: {
+        paddingHorizontal: 8,
+        fontSize: 20,
+        color: 'black'
+    }
+})

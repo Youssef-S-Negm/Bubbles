6abb20395d0ac9d432f845683ee04ab872c14c27
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-06 22:13:32 +0300

    Sign In button component

---
+++ components/buttons/SignInButton.js
@@ -0,0 +1,46 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+
+const SignInButton = () => {
+    return (
+        <TouchableOpacity
+            style={styles.container}
+            onPress={() => {
+                //TODO
+            }}
+        >
+            <LinearGradient
+                colors={['#c4ddfe', '#fed8f7']}
+                start={{ x: 0.3, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                style={styles.gradient}
+            >
+                <View style={styles.buttonText}>
+                    <Text style={styles.signInText}>Sign In</Text>
+                </View>
+            </LinearGradient>
+        </TouchableOpacity>
+    )
+}
+
+export default SignInButton
+
+const styles = StyleSheet.create({
+    container: {
+        width: 252.5
+    },
+    signInText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    gradient: {
+        borderRadius: 6,
+        padding: 2.5,
+    },
+    buttonText: {
+        backgroundColor: 'white',
+        padding: 6,
+        borderRadius: 6
+    }
+})

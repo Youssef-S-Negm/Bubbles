b8ff9dd2040a9b33278092687632dd405d704a15
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-08 19:54:12 +0300

    Implemented onPress callBack function

--- components/buttons/SignUpButton.js
+++ components/buttons/SignUpButton.js
@@ -1,13 +1,14 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
+import { signUp } from '../../db/auth'
 
-const SignUpButton = () => {
-  return (
-    <TouchableOpacity
+const SignUpButton = ({ email, password, confirmPassword }) => {
+    return (
+        <TouchableOpacity
             style={styles.container}
             onPress={() => {
-                //TODO
+                signUp(email, password, confirmPassword)
             }}
         >
             <LinearGradient
@@ -21,7 +22,7 @@ const SignUpButton = () => {
                 </View>
             </LinearGradient>
         </TouchableOpacity>
-  )
+    )
 }
 
 export default SignUpButton

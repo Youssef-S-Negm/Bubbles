ece4af64d9dc7db23245534861de9eb41390a518
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-11 15:06:50 +0300

    Added AuthStack.js file

---
+++ components/screen_stacks/AuthStack.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import SignInScreen from '../screens/SignInScreen'
+import SignUpScreen from '../screens/SignUpScreen'
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen'
+
+const Stack = createNativeStackNavigator()
+
+const AuthStack = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name='Sign In' component={SignInScreen}/>
+            <Stack.Screen name='Sign Up' component={SignUpScreen}/>
+            <Stack.Screen name='Forgot password' component={ForgotPasswordScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default AuthStack

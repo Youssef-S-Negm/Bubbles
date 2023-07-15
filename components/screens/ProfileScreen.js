94f8089a569187900813a668a2464db1de6f4d47
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-15 22:16:10 +0300

    Added QrModal to show the QR code of the current user

--- components/screens/ProfileScreen.js
+++ components/screens/ProfileScreen.js
@@ -1,13 +1,45 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, Modal, StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react'
 import SignOutButton from '../buttons/SignOutButton'
 import ShowQrCodeButton from '../buttons/ShowQrCodeButton'
 import EditProfileButton from '../buttons/EditProfileButton'
 import { AntDesign } from '@expo/vector-icons';
+import QRCode from 'react-native-qrcode-svg'
+import HideQrCodeButton from '../buttons/HideQrCodeButton'
+
+const QrModal = ({ modalVisible, setModalVisible, user }) => {
+    return (
+        <Modal
+            animationType='slide'
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+                setModalVisible(false)
+            }}
+            style={{ flex: 1 }}
+        >
+            <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                    <QRCode
+                        value={user.id}
+                        enableLinearGradient={true}
+                        linearGradient={['#6a00c9', '#00736e']}
+                        size={200}
+                    />
+                    <View style={{ height: 8 }} />
+                    <HideQrCodeButton setModalVisible={setModalVisible} />
+                </View>
+            </View>
+        </Modal>
+    )
+}
 
 const ProfileScreen = ({ user }) => {
+    const [modalVisible, setModalVisible] = useState(false);
+
     return (
         <View style={styles.container}>
+            <QrModal modalVisible={modalVisible} setModalVisible={setModalVisible} user={user} />
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                 <Image
                     source={user.photoURL ? { uri: user.photoURL } : require('../../assets/avatar.png')}
@@ -26,9 +58,9 @@ const ProfileScreen = ({ user }) => {
                 <AntDesign name="phone" style={styles.icon} />
                 <Text style={styles.infoText}>{user.phoneNumber ? user.phoneNumber : 'Unknown'}</Text>
             </View>
-            <View style={{height: 32}}/>
+            <View style={{ height: 32 }} />
             <View style={styles.buttonView}>
-                <ShowQrCodeButton />
+                <ShowQrCodeButton setModalVisible={setModalVisible} />
             </View>
             <View style={styles.buttonView}>
                 <EditProfileButton />
@@ -88,5 +120,20 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'flex-end',
         paddingBottom: 115
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "#fff",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        elevation: 5
     }
 })

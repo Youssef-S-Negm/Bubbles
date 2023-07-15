8117744dce8e7e31687426b5ad1364c451f32d09
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-15 19:30:21 +0300

    Enhanced profile page styling

--- components/screens/ProfileScreen.js
+++ components/screens/ProfileScreen.js
@@ -1,5 +1,9 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import SignOutButton from '../buttons/SignOutButton'
+import ShowQrCodeButton from '../buttons/ShowQrCodeButton'
+import EditProfileButton from '../buttons/EditProfileButton'
+import { AntDesign } from '@expo/vector-icons';
 
 const ProfileScreen = ({ user }) => {
     return (
@@ -12,8 +16,26 @@ const ProfileScreen = ({ user }) => {
                 />
                 <Text style={styles.userName}>{user.displayName}</Text>
             </View>
-            <Text>E-mail: {user.email}</Text>
-            <Text>Phone number: {user.phoneNumber ? user.phoneNumber : 'Unknown'}</Text>
+            <View style={{ height: 8 }} />
+            <View style={styles.infoTextView}>
+                <AntDesign name="mail" style={styles.icon} />
+                <Text style={styles.infoText}>{user.email}</Text>
+            </View>
+            <View style={{ height: 8 }} />
+            <View style={styles.infoTextView}>
+                <AntDesign name="phone" style={styles.icon} />
+                <Text style={styles.infoText}>{user.phoneNumber ? user.phoneNumber : 'Unknown'}</Text>
+            </View>
+            <View style={{height: 32}}/>
+            <View style={styles.buttonView}>
+                <ShowQrCodeButton />
+            </View>
+            <View style={styles.buttonView}>
+                <EditProfileButton />
+            </View>
+            <View style={styles.buttonViewSignOut}>
+                <SignOutButton />
+            </View>
         </View>
     )
 }
@@ -24,7 +46,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 8,
-        paddingHorizontal: 8
+        paddingHorizontal: 8,
+        backgroundColor: 'white'
     },
     image: {
         width: 150,
@@ -34,5 +57,36 @@ const styles = StyleSheet.create({
     userName: {
         paddingLeft: 8,
         fontSize: 25
+    },
+    buttonView: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 8
+    },
+    infoTextView: {
+        width: '100%',
+        flexDirection: 'row',
+        alignContent: 'center',
+        borderRadius: 6,
+        borderWidth: 1,
+        padding: 8
+    },
+    infoText: {
+        fontSize: 20
+    },
+    icon: {
+        fontSize: 20,
+        alignSelf: 'center',
+        paddingRight: 8
+    },
+    buttonViewSignOut: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 8,
+        flex: 1,
+        alignItems: 'flex-end',
+        paddingBottom: 115
     }
 })

9d05985c1dc8e169a6ea9d16820513e17552a51c
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-19 22:36:17 +0300

    added a useState for sent requests

--- components/screen_stacks/ConnectionsStack.js
+++ components/screen_stacks/ConnectionsStack.js
@@ -1,7 +1,7 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import PendingRequestsScreen from "../screens/PendingRequestsScreen";
 import SentRequestsScreen from "../screens/SentRequestsScreen";
-import { getUserById, setPendingRequestsSeenToFalse, userSubscribeListener } from "../../db/users";
+import { getUserById, userSubscribeListener } from "../../db/users";
 import { auth } from "../../db/config";
 import { useEffect, useState } from "react";
 
@@ -9,23 +9,29 @@ const Tab = createMaterialTopTabNavigator()
 
 const ConnectionsStack = () => {
     const [pendingUsers, setPendingUsers] = useState([])
-    
+    const [sentRequestsToUsers, setSentRequestsToUsers] = useState([])
 
     useEffect(() => {
         getUserById(auth.currentUser.uid).then(async currentUser => {
-            const users = await Promise.all(
-                currentUser.pendingRequests.map(getUserById)
-            )
-            setPendingUsers(users)
+            await Promise.all([
+                await Promise.all(currentUser.pendingRequests.map(getUserById)),
+                await Promise.all(currentUser.sentRequests.map(getUserById))
+            ]).then(([pending, sent]) => {
+                setPendingUsers(pending)
+                setSentRequestsToUsers(sent)
+            })
         });
 
         const unsubscribe = userSubscribeListener(auth.currentUser.uid, ({ change }) => {
             if (change.type === "modified") {
                 getUserById(auth.currentUser.uid).then(async currentUser => {
-                    const users = await Promise.all(
-                        currentUser.pendingRequests.map(getUserById)
-                    )
-                    setPendingUsers(users)
+                    await Promise.all([
+                        await Promise.all(currentUser.pendingRequests.map(getUserById)),
+                        await Promise.all(currentUser.sentRequests.map(getUserById))
+                    ]).then(([pending, sent]) => {
+                        setPendingUsers(pending)
+                        setSentRequestsToUsers(sent)
+                    })
                 });
             }
         });
@@ -40,7 +46,9 @@ const ConnectionsStack = () => {
             <Tab.Screen name="Pending requests">
                 {props => <PendingRequestsScreen {...props} pendingUsers={pendingUsers} />}
             </Tab.Screen>
-            <Tab.Screen name="Sent requests" component={SentRequestsScreen} />
+            <Tab.Screen name="Sent requests">
+                {props => <SentRequestsScreen {...props} sentRequests={sentRequestsToUsers} />}
+            </Tab.Screen>
         </Tab.Navigator>
     )
 }

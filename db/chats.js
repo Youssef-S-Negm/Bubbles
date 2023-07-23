c7ef3448fb8ba28640d98a36ac60d5806f598f2e
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-24 02:33:06 +0300

    added sendMessage function

--- db/chats.js
+++ db/chats.js
@@ -1,12 +1,13 @@
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "./config";
+import { arrayUnion, collection, doc, getDoc, onSnapshot, query, serverTimestamp, updateDoc, where } from "firebase/firestore";
+import { auth, db } from "./config";
+import { ToastAndroid } from "react-native";
 
 async function getChatById(id) {
     try {
         const docRef = doc(db, 'chats', id)
         const docSnap = await getDoc(docRef)
         if (docSnap.exists()) {
-            return {...docSnap.data(), id: docSnap.id}
+            return { ...docSnap.data(), id: docSnap.id }
         } else {
             return null
         }
@@ -15,4 +16,29 @@ async function getChatById(id) {
     }
 }
 
-export { getChatById }
+async function sendMessage(chatId, message) {
+    try {
+        const docRef = doc(db, 'chats', chatId)
+        const date = new Date()
+        let formattedDate = date.getHours() + ":" + date.getMinutes()
+        if (date.getHours() < 12) {
+            formattedDate += 'am'
+        } else {
+            formattedDate += 'pm'
+        }
+
+        await updateDoc(docRef, {
+            messages: arrayUnion({
+                message: message,
+                sender: auth.currentUser.uid,
+                sentAt: formattedDate
+            }),
+            updatedAt: serverTimestamp()
+        })
+    } catch (err) {
+        console.log('Error sending message:', err);
+        ToastAndroid.show("Couldn't send the message", ToastAndroid.SHORT)
+    }
+}
+
+export { getChatById, sendMessage }

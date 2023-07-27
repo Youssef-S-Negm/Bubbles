c38f0b277105d619fe143a3cc2bbd6af11796254
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-27 22:23:05 +0300

    added a background color to images in ProfileTab

--- components/tabs/ProfileTab.js
+++ components/tabs/ProfileTab.js
@@ -40,6 +40,7 @@ const styles = StyleSheet.create({
     image: {
         width: 35,
         height: 35,
-        borderRadius: 30
+        borderRadius: 30,
+        backgroundColor: 'white'
     }
 })

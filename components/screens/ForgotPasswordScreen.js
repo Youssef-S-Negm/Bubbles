5f98bafd2e4e64bc4f284f3e51df55e7f7e95ea9
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-29 00:53:27 +0300

    enahnced ForgotPasswordScreen

--- components/screens/ForgotPasswordScreen.js
+++ components/screens/ForgotPasswordScreen.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import { useState } from 'react'
 import SendEmailButton from '../buttons/SendEmailButton'
+import { AntDesign } from '@expo/vector-icons'
 
 const ForgotPasswordScreen = ({ navigation }) => {
     const [email, setEmail] = useState('')
@@ -9,13 +10,16 @@ const ForgotPasswordScreen = ({ navigation }) => {
         <View style={styles.container}>
             <Text>Please, enter your E-mail</Text>
             <View style={{ height: 8 }} />
-            <TextInput
-                placeholder='user@example.com'
-                style={styles.textInput}
-                value={email}
-                onChangeText={e => setEmail(e)}
-                keyboardType='email-address'
-            />
+            <View style={styles.inputView}>
+                <AntDesign name='mail' style={styles.icon}/>
+                <TextInput
+                    placeholder='user@example.com'
+                    style={styles.textInput}
+                    value={email}
+                    onChangeText={e => setEmail(e)}
+                    keyboardType='email-address'
+                />
+            </View>
             <View style={{ height: 32 }} />
             <View style={styles.buttonView}>
                 <SendEmailButton email={email} navigation={navigation} />
@@ -28,17 +32,28 @@ export default ForgotPasswordScreen
 
 const styles = StyleSheet.create({
     textInput: {
-        alignSelf: 'stretch',
-        borderWidth: 1,
-        padding: 8,
-        borderRadius: 6,
+        width: '100%',
+        marginLeft: 8
     },
     container: {
         flex: 1,
-        paddingTop: 32
+        backgroundColor: 'white',
+        paddingTop: 8,
+        paddingHorizontal: 8
     },
     buttonView: {
         flexDirection: 'row',
         justifyContent: 'center'
+    },
+    inputView: {
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 6,
+        padding: 8
+    },
+    icon: {
+        fontSize: 20
     }
 })

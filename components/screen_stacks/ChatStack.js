0216f898a9ac4ad89578b8dc297ff87a23fc1e95
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-30 18:44:26 +0300

    added screens to ChatStack: -AddUserToGroupChatScreen -GroupChatInfoScreen

--- components/screen_stacks/ChatStack.js
+++ components/screen_stacks/ChatStack.js
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import ChatScreen from "../screens/ChatScreen";
 import CreateGroupChatScreen from "../screens/CreateGroupChatScreen";
+import AddUserToGroupChatScreen from "../screens/AddUserToGroupChatScreen";
+import GroupChatInfoScreen from "../screens/GroupChatInfoScreen";
 
 const Stack = createNativeStackNavigator()
 
@@ -11,6 +13,8 @@ const ChatStack = () => {
             <Stack.Screen name="Chats" component={HomeScreen} />
             <Stack.Screen name="Conversation" component={ChatScreen} />
             <Stack.Screen name="Create group chat" component={CreateGroupChatScreen} />
+            <Stack.Screen name="Add user" component={AddUserToGroupChatScreen} />
+            <Stack.Screen name="Group info" component={GroupChatInfoScreen} />
         </Stack.Navigator>
     )
 }

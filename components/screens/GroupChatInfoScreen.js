103c734105f741514571f5131da2d88c4357f2fe
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-07-30 18:44:19 +0300

    added GroupChatInfoScreen to components/screens (not implemented yet)

---
+++ components/screens/GroupChatInfoScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GroupChatInfoScreen = () => {
+  return (
+    <View>
+      <Text>GroupChatInfoScreen</Text>
+    </View>
+  )
+}
+
+export default GroupChatInfoScreen
+
+const styles = StyleSheet.create({})

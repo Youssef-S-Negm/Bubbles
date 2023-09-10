c1dccfb9bd5fbabf48de80c02f203cc623ec626d
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-09-10 23:53:36 +0300

    passed required props to attachments screens in ChatAttachmentsStack.js

--- components/screen_stacks/ChatAttachmentsStack.js
+++ components/screen_stacks/ChatAttachmentsStack.js
@@ -1,13 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text } from 'react-native'
+import { useEffect, useState } from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import AllChatAttachmentsScreen from '../screens/AllChatAttachmentsScreen'
 import SavedChatAttachmentsScreen from '../screens/SavedChatAttachmentsScreen'
 import UnsavedChatAttachmentsScreen from '../screens/UnsavedChatAttachmentsScreen'
+import * as FileSystem from 'expo-file-system'
 
 const Tab = createMaterialTopTabNavigator()
 
-const ChatAttachmentsStack = () => {
+const ChatAttachmentsStack = ({ route }) => {
+    const { chat } = route.params
+    const [savedAttachments, setSavedAttachments] = useState([])
+    const [UnsavedAttachments, setUnsavedAttachments] = useState([])
+    const [updated, setUpdated] = useState(false)
+    const attachmentsDirectory = `${FileSystem.documentDirectory}/${chat.id}/attachments/`
+
+    const doesExist = async (fileName) => {
+        const directory = await FileSystem.getInfoAsync(attachmentsDirectory + fileName)
+        return directory.exists
+    }
+
+    const filterAttachments = () => {
+        const saved = []
+        const unsaved = []
+        chat.allAttachments.forEach(async attachment => {
+            if (await doesExist(attachment.name)) {
+                saved.push(attachment)
+            } else {
+                unsaved.push(attachment)
+            }
+        });
+        setSavedAttachments(saved)
+        setUnsavedAttachments(unsaved)
+    }
+
+    useEffect(() => {
+        filterAttachments()
+    }, [updated])
+
     return (
         <Tab.Navigator
             screenOptions={({ route }) => ({
@@ -51,9 +81,33 @@ const ChatAttachmentsStack = () => {
                 }
             })}
         >
-            <Tab.Screen name='All attachments' component={AllChatAttachmentsScreen} />
-            <Tab.Screen name='Saved attachments' component={SavedChatAttachmentsScreen} />
-            <Tab.Screen name='Unsaved attachments' component={UnsavedChatAttachmentsScreen} />
+            <Tab.Screen name='All attachments'>
+                {props => <AllChatAttachmentsScreen
+                    {...props}
+                    attachments={chat.allAttachments}
+                    chatId={chat.id}
+                    setUpdated={setUpdated}
+                    updated={updated}
+                />}
+            </Tab.Screen>
+            <Tab.Screen name='Saved attachments'>
+                {props => <SavedChatAttachmentsScreen
+                    {...props}
+                    attachments={savedAttachments}
+                    chatId={chat.id}
+                    setUpdated={setUpdated}
+                    updated={updated}
+                />}
+            </Tab.Screen>
+            <Tab.Screen name='Unsaved attachments'>
+                {props => <UnsavedChatAttachmentsScreen
+                    {...props}
+                    attachments={UnsavedAttachments}
+                    chatId={chat.id}
+                    setUpdated={setUpdated}
+                    updated={updated}
+                />}
+            </Tab.Screen>
         </Tab.Navigator>
     )
 }
